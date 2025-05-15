ba454b70b4b4be20ea77b0dd4bc86df7d6c2446e
MDKL-Bowi <muhammaddwikurnialubis@gmail.com>
2025-05-15 11:10:55 +0700

    Update hash.php

--- hash.php
+++ hash.php
@@ -1,5 +1,5 @@
 <?php
-$password = "superuser"; // The password you want to hash
+$password = "password"; // The password to hash
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Hash the password
 
 echo "Hashed Password: " . $hashedPassword;
