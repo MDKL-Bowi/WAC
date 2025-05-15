a3f5b37fde10dfb76c33124b4905facb73efabc5
MDKL-Bowi <muhammaddwikurnialubis@gmail.com>
2025-05-15 10:13:45 +0700

    Add files via upload

---
+++ includes/session.php
@@ -0,0 +1,25 @@
+<?php
+// Start or resume the session
+session_start();
+
+// Check if the user is logged in
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
+}
+
+// Redirect to login page if the user is not logged in
+function requireLogin() {
+    if (!isLoggedIn()) {
+        header("Location: login.php");
+        exit();
+    }
+}
+
+// Redirect to home page if the user is already logged in
+function requireGuest() {
+    if (isLoggedIn()) {
+        header("Location: index.php");
+        exit();
+    }
+}
+?>
